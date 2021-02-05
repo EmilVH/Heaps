3efa9175f5123a32303b6f2e341ecc72686bd08f
EmilVH <58426580+EmilVH@users.noreply.github.com>
2021-02-05 11:32:32 +0300

    deleted useless files

--- Heaps/cmake-build-debug/Google_tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vahna/Desktop/Heaps/Heaps/Heaps/Google_tests
-# Build directory: C:/Users/vahna/Desktop/Heaps/Heaps/Heaps/cmake-build-debug/Google_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- Heaps/cmake-build-debug/Google_tests/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vahna/Desktop/Heaps/Heaps/Heaps/Google_tests/googlemock
-# Build directory: C:/Users/vahna/Desktop/Heaps/Heaps/Heaps/cmake-build-debug/Google_tests/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
